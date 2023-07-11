0824c1de7636cb98bcc94a0f4d70d0f3482df881
hyeontak <leehyeontak@naver.com>
2023-07-11 18:05:13 +0900

    최초

---
+++ cTak/src/main/java/com/dev/cTak/CTakApplication.java
@@ -0,0 +1,13 @@
+package com.dev.cTak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTakApplication.class, args);
+	}
+
+}
